20c92de72eb31aef33cfd7fc583b920901593aef
Dione-b <dionenassau321@gmail.com>
2021-07-12 18:02:55 -0300

    portifolio urna

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Aula-Javascript
-Introdução ao Javascript
+# Portfolio
+
+#### Projeto de Urna eletrônica
+
+##### Candidatos:
+
+- Prefeito  - **99** (_Lisa Simpson_) | Vice-Prefeito (_Mil House_)
+- Prefeito  - **84** (_Mr. Burns_) | Vice-Prefeito (_Dr. Skinner_)
+- Vereador - **38111** (_Homer Simpson_)
+- Vereador - **77222** (_Bart Simpson_)
+
---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Homer Simpson',
+                partido: 'PL',
+                fotos: [
+                    { url: '38111.png', legenda: 'VEREADOR', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Bart Simpson',
+                partido: 'PSOL',
+                fotos: [
+                    { url: '77222.png', legenda: 'VEREADOR', }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Lisa Simpson',
+                partido: 'PCdB',
+                vice: 'Mil House M.',
+                fotos: [
+                    { url: '99.png', legenda: 'PREFEITA' },
+                    { url: '99_2.png', legenda: 'VICE-PREFEITO', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'MR. Burns',
+                partido: 'PT',
+                vice: 'Diretor Skiner',
+                fotos: [
+                    { url: '84.png', legenda: 'PREFEITO' },
+                    { url: '84_2.png', legenda: 'VICE-PREFEITO', small: true }
+                ]
+            },
+        ]
+    }
+];
